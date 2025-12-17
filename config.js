e70295794b48f71976edbef4918dcc17703a06f2
Cacheplows-alt <cacheplows@gmail.com>
2025-12-17 10:05:02 -0700

    Refactor form backend to use config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    /**
+     * INSTRUCTIONS FOR USER:
+     * 1. Deploy your Google Apps Script (backend.gs).
+     * 2. Copy the "Web App URL" (starts with https://script.google.com/macros/s/...).
+     * 3. Paste it inside the quotes below, replacing the placeholder.
+     */
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxcmBXXwNmqCpH00tUv4srcAmVafG5BfNpWwc1ztsMZKUELYsQj0Iv4AU2eCDTE2jjS/exec'
+};
